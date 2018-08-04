070d66c9bfb9a23d82bdaf7b44ba5c52c3706dca
Bingnan Li, Nathan <leebingnam@hotmail.com>
2018-08-04 16:59:17 -0700

    IO working on progress.

--- Assets/Script/InputHandler.cs
+++ Assets/Script/InputHandler.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour {
+[RequireComponent(typeof(PlayerStatus))]
+[RequireComponent(typeof(Rigidbody))]
+public class InputHandler : MonoBehaviour
+{
+    private PlayerStatus playerStatus;
+    [SerializeField]
+    private float jumpSpeed = 8.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        playerStatus = transform.GetComponent<PlayerStatus>();
+    }
+
+    public void MoveLeft()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+    }
+
+    public void Jump()
+    {
+        if (playerStatus.isGrounded)
+        {
+
+        }
+    }
+
+    public void MakePoseLeft()
+    {
+        playerStatus.currentPose = PlayerStatus.PlayerPose.WiiFit_Left;
+    }
+
+    public void MakePoseRight()
+    {
+        playerStatus.currentPose = PlayerStatus.PlayerPose.Mickeal_Right;
+    }
+
+    public void MakePoseUp()
+    {
+        playerStatus.currentPose = PlayerStatus.PlayerPose.Yoga_Up;
+    }
+
+    public void MakePoseDown()
+    {
+        playerStatus.currentPose = PlayerStatus.PlayerPose.SpiltFoot_Down;
+    }
 }
---
+++ Assets/Script/PlayerStatus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public readonly float initialSpeed = 1.0f;
+    public readonly float maxSpeed = 100.0f;
+    private float currentSpeed;
+
+    public enum PlayerPose
+    {
+        Runing,
+        Yoga_Up,
+        Mickeal_Right,
+        WiiFit_Left,
+        SpiltFoot_Down
+    }
+    public PlayerPose currentPose = PlayerPose.Runing;
+
+    public bool isGrounded { get { return isGrounded;} }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    
+}
